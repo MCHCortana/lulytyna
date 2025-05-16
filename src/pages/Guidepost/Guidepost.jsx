dc6e39f3639ae3df11478768dd942a28ef997364
MCHCortana <wolokika@gmail.com>
2025-05-16 20:48:43 +0200

    fixed lightsaber

--- src/pages/Guidepost/Guidepost.jsx
+++ src/pages/Guidepost/Guidepost.jsx
@@ -4,9 +4,9 @@ import {Lightsaber} from "../../components/Lightsaber/Lightsaber.jsx";
 
 export const Guidepost = () => {
     return (
-        <>
+        <section className="guide-post">
             <Header skipAnimation={true}/>
             <Lightsaber />
-        </>)
+        </section>)
 
 }
